016b6e5b68df8836abd28ba976f7b08629183493
Jared Thomas <jcjt929@web-development-teaching.com>
2022-05-12 00:36:09 -0400

    share itineraries with users

--- server/controller/routes.js
+++ server/controller/routes.js
@@ -68,7 +68,7 @@ module.exports = (app, passport, db) => {
 	});
 
 	app.post('/api/itinerary/shared', function(req,res){
-		var query = `INSERT INTO shared_itineraries (owner_id, requester_id, itinerary_id) VALUES ('${req.body.owner_id}','${req.body.requester_id}','${req.body.itinerary_id}')`;
+		var query = `INSERT INTO shared_itineraries (owner_id, requester_id, itinerary_id, approved) VALUES ('${req.body.owner_id}','${req.body.requester_id}','${req.body.itinerary_id}', ${req.body.approved ? 1 : 0})`;
 		db.query(query, (error,queryRes) => {
 			if(error){
 				res.json({success: false, error: error})
@@ -78,35 +78,63 @@ module.exports = (app, passport, db) => {
 		});
 	});
 
+	app.put('/api/itinerary/shared', function(req,res){
+		if(req.body.approved){
+			var query = `UPDATE shared_itineraries SET approved=1 WHERE itinerary_id=${req.body.itinerary_id} AND requester_id=${req.body.requester_id}`;
+			db.query(query, (error,queryRes) => {
+				if(error){
+					res.json({success: false, error: error})
+				} else {
+					res.json({success: true, itinerary_id: req.body.itinerary_id})
+				}
+			});
+		} else {
+			var query = `DELETE FROM shared_itineraries WHERE itinerary_id=${req.body.itinerary_id} AND requester_id=${req.body.requester_id}`;
+			db.query(query, (error,queryRes) => {
+				if(error){
+					res.json({success: false, error: error})
+				} else {
+					res.json({success: true, itinerary_id: req.body.itinerary_id})
+				}
+			});
+		}
+	});
+
 	app.get('/api/itinerary/shared', function(req,res){
-		var query = `SELECT itinerary_id,owner_id FROM shared_itineraries WHERE requester_id=${req.session.passport.user.id}`;
+		const approved = req.query.approved == "true" ? 1 : 0;
+		var query = `SELECT itinerary_id,owner_id FROM shared_itineraries WHERE requester_id=${req.session.passport.user.id} AND approved=${approved}`;
 		db.query(query, (error,queryRes) => {
 			if(error){
 				res.json({success: false, error: error})
 			} else {
-				const sharedItineraryIds = queryRes.map((res) => res.itinerary_id);
-				query = `SELECT * FROM itinerary WHERE id in (${sharedItineraryIds.join(",")})`;
-				db.query(query, (error,itineraryQueryRes) => {
-					if(error){
-						res.json({success: false, error: error})
-					} else {
-						query = `SELECT id,email FROM users`;
-						db.query(query, (error,userQueryRes) => {
-							if(error){
-								res.json({success: false, error: error})
-							} else {
-								itineraryQueryRes.forEach((itinerary) => {
-									userQueryRes.forEach((user)=> {
-										if(itinerary.user_id == user.id){
-											itinerary.owner_email = user.email;
-										}
+				if(queryRes.length > 0){
+					let sharedItineraryIds = queryRes.map((res) => res.itinerary_id);
+					sharedItineraryIds = [...new Set(sharedItineraryIds)]
+					query = `SELECT * FROM itinerary WHERE id in (${sharedItineraryIds.join(",")})`;
+					db.query(query, (error,itineraryQueryRes) => {
+						if(error){
+							res.json({success: false, error: error})
+						} else {
+							query = `SELECT id,email FROM users`;
+							db.query(query, (error,userQueryRes) => {
+								if(error){
+									res.json({success: false, error: error})
+								} else {
+									itineraryQueryRes.forEach((itinerary) => {
+										userQueryRes.forEach((user)=> {
+											if(itinerary.user_id == user.id){
+												itinerary.owner_email = user.email;
+											}
+										})
 									})
-								})
-								res.json({success: true, itineraries: itineraryQueryRes})
-							}
-						});
-					}
-				});
+									res.json({success: true, itineraries: itineraryQueryRes})
+								}
+							});
+						}
+					});
+				} else {
+					res.json({success: true, itineraries: []})
+				}
 			}
 		});
 	});
@@ -120,8 +148,6 @@ module.exports = (app, passport, db) => {
 			}
 		})(req, res, next);
 	});
-
-
 
 	app.post('/api/sign-in', function(req,res,next){
 		passport.authenticate('local-signin', function(err, user, info){
